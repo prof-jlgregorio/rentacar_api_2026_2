08b56ce2684bd499ac977016a8193242afa7b7b9
J. L. Gregório <jlgregorio81@hotmail.com>
2026-09-23 08:40:04 -0300

    Documentation of Manufacturer Controller - The beginning

--- src/main/java/br/com/jlgregorio/rentacar/controller/ManufacturerController.java
+++ src/main/java/br/com/jlgregorio/rentacar/controller/ManufacturerController.java
@@ -1,13 +1,21 @@
 package br.com.jlgregorio.rentacar.controller;
 
 import br.com.jlgregorio.rentacar.dto.ManufacturerDto;
+import br.com.jlgregorio.rentacar.exception.CustomExceptionResponse;
 import br.com.jlgregorio.rentacar.service.ManufacturerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Manufacturers", description = "Endpoint para CRUD de Manufacturers")
 @RestController
 @RequestMapping("/manufacturers/v1")
 public class ManufacturerController {
@@ -18,13 +26,28 @@ public class ManufacturerController {
         this.manufacturerService = manufacturerService;
     }
 
+
     @PostMapping
+    @Operation(summary = "Cria um Manufacturer no banco de dados", responses = {
+            @ApiResponse(description = "O Manufacturer criado", responseCode = "201", content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    schema = @Schema(implementation = ManufacturerDto.class)
+            ))
+    })
     public ResponseEntity<ManufacturerDto> create(@RequestBody ManufacturerDto dto){
         var created = manufacturerService.create(dto);
         return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Recuperar um Manufacturer mediante o ID informado", responses = {
+            @ApiResponse(description = "O Manufacturer localizado, caso ele exista", responseCode = "200",
+            content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE,
+            schema = @Schema(implementation = ManufacturerDto.class))),
+            @ApiResponse(description = "Manufacturer não encontrado - ID não existe", responseCode = "404",
+            content = @Content(mediaType = MediaType.MULTIPART_FORM_DATA_VALUE,
+            schema = @Schema(implementation = CustomExceptionResponse.class)))
+    })
     public ResponseEntity<ManufacturerDto> findById(@PathVariable Long id){
         var found = manufacturerService.findById(id);
         return new ResponseEntity<>(found, HttpStatus.OK);
